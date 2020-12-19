e77b3b9d1d76d1229f63b28ed7db12ae0d4c5c6b
leventarican <levent@protonmail.com>
2020-12-19 23:10:59 +0100

    basic logic without viewmodel

--- examples/app_viewmodel/app/src/main/java/com/github/leventarican/app_viewmodel/MainActivity.kt
+++ examples/app_viewmodel/app/src/main/java/com/github/leventarican/app_viewmodel/MainActivity.kt
@@ -2,10 +2,29 @@ package com.github.leventarican.app_viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var value = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun increase(v: View) {
+        value++;
+        display();
+    }
+
+    fun decrease(v: View) {
+        value--;
+        display();
+    }
+
+    private fun display() {
+        val tv = findViewById<TextView>(R.id.textView);
+        tv.text = value.toString();
     }
 }
