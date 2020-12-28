a0eb37897282bb039e2083ee7ddebba02bd6e2e0
leventarican <levent@protonmail.com>
2020-12-28 23:50:36 +0100

    data binding example

--- examples/AppLayout/app/src/main/java/com/github/leventarican/applayout/MainActivity.kt
+++ examples/AppLayout/app/src/main/java/com/github/leventarican/applayout/MainActivity.kt
@@ -8,26 +8,38 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.github.leventarican.applayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // ActivityMainBinding name is derived from the layout file: activity_main
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // connect layout file with activity
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button).setOnClickListener { process(it) }
+        // use data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//        findViewById<Button>(R.id.button).setOnClickListener { process(it) }
+        binding.button.setOnClickListener { process(it) }
     }
 
     private fun process(view: View?) {
-        val e = findViewById<EditText>(R.id.edit)
-        val tv = findViewById<TextView>(R.id.textView)
+//        val e = findViewById<EditText>(R.id.edit)
+//        val tv = findViewById<TextView>(R.id.textView)
+//
+//        tv.text = e.text
 
-        tv.text = e.text
+        binding.textView.text = binding.edit.text
 
         hideKeyboard(view)
     }
-    
+
     private fun hideKeyboard(view: View?) {
         val keyboard =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
