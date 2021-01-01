9d9d549fac0df993cbe7c1c129e282dc98e66763
leventarican <levent@protonmail.com>
2021-01-01 17:45:43 +0100

    just prototyping

---
+++ examples/AppLe/README.md
@@ -0,0 +1,7 @@
+# App Le
+
+## data binding
+* make a data class directly available to a view
+
+## links
+* https://developer.android.com/courses/kotlin-android-fundamentals/toc
--- examples/AppLe/app/src/main/java/com/github/leventarican/apple/MainActivity.kt
+++ examples/AppLe/app/src/main/java/com/github/leventarican/apple/MainActivity.kt
@@ -6,6 +6,16 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // without data binding
+//        setContentView(R.layout.activity_main)
+
+        // with data binding
+        // https://github.com/google-developer-training/android-kotlin-fundamentals-apps/blob/master/AboutMeDataBinding/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // for fragments
+        // https://developer.android.com/codelabs/kotlin-android-training-create-and-add-fragment?index=..%2F..android-kotlin-fundamentals#3
+//        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,container,false)
     }
 }
