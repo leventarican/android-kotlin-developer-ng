1d16e49edeab693dc81ea451d4b8a6f79012946e
leventarican <levent@protonmail.com>
2021-01-29 21:28:40 +0100

    finalize navigation component example

--- examples/AppLe/app/src/main/java/com/github/leventarican/apple/Screen0.kt
+++ examples/AppLe/app/src/main/java/com/github/leventarican/apple/Screen0.kt
@@ -1,13 +1,12 @@
 package com.github.leventarican.apple
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import com.github.leventarican.apple.R.id.action_screen0_to_screen1
 import com.github.leventarican.apple.databinding.FragmentScreen0Binding
 
 class Screen0 : Fragment() {
@@ -18,7 +17,7 @@ class Screen0 : Fragment() {
         val binding: FragmentScreen0Binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_screen0, container, false)
 
-        binding.button0.setOnClickListener { Navigation.findNavController(action_screen0_to_screen1) }
+        binding.button0.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_screen0_to_screen1))
 
         return binding.root
     }
