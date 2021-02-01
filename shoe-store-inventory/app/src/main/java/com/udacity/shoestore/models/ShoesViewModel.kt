0b9e47d833e09ac35ee115f29780b3e3ebe029fe
leventarican <levent@protonmail.com>
2021-02-01 23:14:36 +0100

    use viewmodel

--- shoe-store-inventory/app/src/main/java/com/udacity/shoestore/models/ShoesViewModel.kt
+++ shoe-store-inventory/app/src/main/java/com/udacity/shoestore/models/ShoesViewModel.kt
@@ -1,19 +1,32 @@
 package com.udacity.shoestore.models
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoesViewModel : ViewModel() {
     private lateinit var shoeList: MutableList<Shoe>
 
-    init {
-        fillShoes()
+    private val _shoes = MutableLiveData<List<Shoe>>()
+    val shoes: LiveData<List<Shoe>>
+        get() = _shoes
+
+    fun storeShoe(shoe: Shoe) {
+        shoe.let {
+            shoeList.add(it)
+            _shoes.value = shoeList
+        }
     }
 
-    private fun fillShoes() {
-        shoeList = mutableListOf(
-            Shoe("java", 15.0, ".java", "buy once wear everywhere"),
-            Shoe("kotlin", 1.3, ".kt", "concise"),
-            Shoe("rust", 1.49, ".rs", "safe")
-        )
-    }
+//    init {
+//        fillShoes()
+//    }
+//
+//    private fun fillShoes() {
+//        shoeList = mutableListOf(
+//            Shoe("java", 15.0, ".java", "buy once wear everywhere"),
+//            Shoe("kotlin", 1.3, ".kt", "concise"),
+//            Shoe("rust", 1.49, ".rs", "safe")
+//        )
+//    }
 }
--- shoe-store-inventory/app/src/main/java/com/udacity/shoestore/screens/ListingFragment.kt
+++ shoe-store-inventory/app/src/main/java/com/udacity/shoestore/screens/ListingFragment.kt
@@ -5,20 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentListingBinding
 import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.models.ShoesViewModel
 
 class ListingFragment : Fragment() {
+
+    private lateinit var viewModel: ShoesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentListingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_listing, container, false)
+        val binding: FragmentListingBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_listing, container, false)
+
+        viewModel = ViewModelProvider(this).get(ShoesViewModel::class.java)
+
+        // data binding
+        binding.shoesViewModel = viewModel
+
+        viewModel.shoes.observe(viewLifecycleOwner, Observer {
+            it.forEach { shoe -> binding.llListing.addView(textViewFrom(shoe)) }
+        })
 
         return binding.root
+    }
+
+    private fun textViewFrom(shoe: Shoe): TextView {
+        val tv = TextView(context)
+        tv.text = shoe.name
+        return tv
     }
 
 }
