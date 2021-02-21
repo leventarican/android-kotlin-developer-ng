21caa91dd174d29cca1888aa476995704e1cd7a9
leventarican <levent@protonmail.com>
2021-02-21 23:04:06 +0100

    connect UI with db

--- examples/room/app/src/main/java/com/github/leventarican/room/MainActivity.kt
+++ examples/room/app/src/main/java/com/github/leventarican/room/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.github.leventarican.room
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.leventarican.room.recyclerview.ProgrammingLanguageListAdapter
+import com.github.leventarican.room.room.ProgrammingLanguage
+import com.github.leventarican.room.viewmodel.ProgrammingLanguageViewModel
+import com.github.leventarican.room.viewmodel.ProgrammingLanguageViewModelFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newProgrammingLanguageActivityRequestCode = 1
+    private val programmingLanguageViewModel: ProgrammingLanguageViewModel by viewModels {
+        ProgrammingLanguageViewModelFactory((application as ProgrammingLanguageApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +28,31 @@ class MainActivity : AppCompatActivity() {
         val adapter = ProgrammingLanguageListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        programmingLanguageViewModel.allLanguages.observe(this) {
+            it.let {
+                adapter.submitList(it)
+            }
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewProgrammingLanguageActivity::class.java)
+            startActivityForResult(intent, newProgrammingLanguageActivityRequestCode)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        if (requestCode == newProgrammingLanguageActivityRequestCode && resultCode == RESULT_OK) {
+            intentData?.getStringExtra(NewProgrammingLanguageActivity.EXTRA_REPLY)?.let { reply ->
+                val programmingLanguage = ProgrammingLanguage(reply)
+                programmingLanguageViewModel.insert(programmingLanguage)
+            }
+        } else {
+            Toast.makeText(applicationContext, "not saved", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
--- examples/room/app/src/main/java/com/github/leventarican/room/room/ProgrammingLanguage.kt
+++ examples/room/app/src/main/java/com/github/leventarican/room/room/ProgrammingLanguage.kt
@@ -2,9 +2,10 @@ package com.github.leventarican.room.room
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * add annotation to connect class and database for android room
  */
 @Entity(tableName="programming_language")
-data class ProgrammingLanguage(@ColumnInfo val language: String)
+data class ProgrammingLanguage(@PrimaryKey @ColumnInfo val language: String)
