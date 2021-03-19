976f605a08dd119ec66fdb826884c93a1692bc25
leventarican <levent@protonmail.com>
2021-03-19 16:26:41 +0100

    add options menu

--- examples/underconstruction/app/src/main/java/com/github/leventarican/underconstruction/MainActivity.kt
+++ examples/underconstruction/app/src/main/java/com/github/leventarican/underconstruction/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.github.leventarican.underconstruction
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import com.github.leventarican.underconstruction.databinding.ActivityMainBinding
 
 /**
+ * TODO: implement options menu (incl. menu.xml)
+ * DONE: implement navigation UP with Navigation UI
  * DONE: back button does not work. reset data to 0. if not it will navigate always to screen 1
  */
 class MainActivity : AppCompatActivity() {
@@ -32,7 +38,24 @@ class MainActivity : AppCompatActivity() {
         // next, we want to add navigation to the app bar.
         // this means we will have a UP button in the app bar.
         // to do this we say this is the activity and this is the navigation host.
+        // we also need to override onSupportNavigateUp() method.
         NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.navigationHost))
+
+        // next, we want also a options menu / overflow menu in the app bar.
+        // for that purpose we implement onCreateOptionsMenu() to show the menu
+        // and onOptionsItemSelected to implement item -> destination.
+        // NOTE, that menu item id (navigation_menu.xml) is same as navigation destination id (navigation.xml)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.navigation_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.navigationHost)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
